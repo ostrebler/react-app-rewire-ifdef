bb0720b44d1dff60086cb46113a9c3ca9570b196
ostrebler <strebler.olivier@hotmail.fr>
2019-09-14 23:36:13 +0200

    commit

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
+const { getBabelLoader, addWebpackModuleRule } = require('customize-cra')
+
 module.exports = ({ "ifdef-env": autoEnv, ...options } = {}) => config => {
-  const loaders = config.module.rules.find(rule => Array.isArray(rule.oneOf)).oneOf
-  const index = loaders.findIndex(({ loader }) => typeof loader === 'string' && loader.includes('babel-loader'))
-  const { loader, options, ...rest } = loaders[index]
-  loaders[index] = {
-    ...rest,
-    use: [{ loader, options }, {
-      loader: 'ifdef-loader',
-      options: {
-        ...autoEnv ? process.env : {},
-        ...options
-      }
-    }]
-  }
-  return config
+  const { test, include } = getBabelLoader(config)
+  return addWebpackModuleRule({
+    test,
+    include,
+    loader: 'ifdef-loader',
+    options: {
+      ...autoEnv ? process.env : {},
+      ...options
+    }
+  })(config)
 }
